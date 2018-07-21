e5dd9a82a6e9bb62fe3dfd813824e849828e1cab
Kittiporn Korprasertthaworn <izporn@gmail.com>
2018-07-21 14:52:30 +0700

    Finish package, var

--- stringhelper/helper.go
+++ stringhelper/helper.go
@@ -1,6 +1,19 @@
 package stringhelper
 
+import (
+	"fmt"
+)
+
 // Upper return CAT
 func Upper(text string) string {
 	return "CAT"
+}
+
+// Concat string
+func Concat(inputs ...string) string {
+	var result string
+	for _, input := range inputs {
+		result = fmt.Sprintf("%s %s", result, input)
+	}
+	return result
 }
