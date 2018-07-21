0357c25e418f267a9fe5ac6a62034283c37d815d
Kittiporn Korprasertthaworn <izporn@gmail.com>
2018-07-21 14:28:35 +0700

    Init

---
+++ calculator.go
@@ -0,0 +1,6 @@
+package gotools
+
+// Add will return x+y
+func Add(x, y int) int {
+	return x + y
+}
---
+++ stringhelper/helper.go
@@ -0,0 +1,6 @@
+package stringhelper
+
+// Upper return CAT
+func Upper(text string) string {
+	return "CAT"
+}
